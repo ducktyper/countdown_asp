ec24514282194c7500c10ade2a3ee1ef2b6eac79
ducktyper <ducktyper@gmail.com>
2015-07-28 21:26:25 +1200

    Create new blank web project

---
+++ Countdown.Tests/StoreTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Countdown.Tests
+{
+    [TestClass]
+    public class StoreTest
+    {
+        [TestMethod]
+        public void TestAddItem()
+        {
+        }
+    }
+}
