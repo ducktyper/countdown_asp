2b660cb56620f749c97d20d54c826ccaef69d8ad
ducktyper <ducktyper@gmail.com>
2015-07-28 22:24:13 +1200

    CalculateCost

--- Countdown.Tests/StoreTest.cs
+++ Countdown.Tests/StoreTest.cs
@@ -22,5 +22,14 @@ namespace Countdown.Tests
             store.AddItem("0002", "orange", 10);
             Assert.AreEqual(2, store.ItemCount());
         }
+
+        [TestMethod]
+        public void TestCalculateCost()
+        {
+            Store store = new Store();
+            store.AddItem("0001", "apple", 5);
+            store.AddItem("0002", "orange", 10);
+            Assert.AreEqual(15, store.CalculateCost(new string[] {"0001", "0002"}));
+        }
     }
 }
--- Countdown/Store.cs
+++ Countdown/Store.cs
@@ -8,20 +8,35 @@ namespace Countdown
     public class Store
     {
         private List<string> barcodes;
+        private List<int> prices;
 
         public Store()
         {
             barcodes = new List<string>();
+            prices = new List<int>();
         }
 
         public void AddItem(string barcode, string name, int price)
         {
             barcodes.Add(barcode);
+            prices.Add(price);
         }
 
         public int ItemCount()
         {
             return barcodes.Count();
+        }
+
+        public int CalculateCost(string[] barcodes)
+        {
+            int total = 0;
+            foreach (string barcode in barcodes)
+            {
+                int index = Array.IndexOf(barcodes, barcode);
+                int price = prices.ElementAt(index);
+                total += price;
+            }
+            return total;
         }
     }
 }
