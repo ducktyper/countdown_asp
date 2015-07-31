97841e11893d3db892e2f9269c36b1608f5bedd5
ducktyper <ducktyper@gmail.com>
2015-07-31 21:47:34 +1200

    Refactor purchase summary

--- Countdown/Store.cs
+++ Countdown/Store.cs
@@ -12,86 +12,99 @@ namespace Countdown
         public string name;
         public float price;
     }
-    public struct Purchase
+    public class Purchase
     {
-        public Product[] products;
-        public DateTime purchased_at;
+        public Product[] products {get; private set;}
+        public DateTime purchased_at {get; private set;}
+        public Purchase(Product[] _products)
+        {
+            products     = _products;
+            purchased_at = DateTime.Now;
+        }
+        public string DisplayTime()
+        {
+            return String.Format("{0:MM dd YYYY}", purchased_at);
+        }
+        public int ProductCount()
+        {
+            return products.Count();
+        }
+        public float Cost()
+        {
+            return products.Select(b => b.price).Sum();
+        }
+    }
+    public class SummaryBuilder
+    {
+        public ArrayList summary {get; private set;}
+        public SummaryBuilder()
+        {
+            summary = new ArrayList();
+            AddHeaders();
+        }
+        public void Add(string time, int count,  float cost)
+        {
+            summary.Add(new string[] {time, count.ToString(), cost.ToString()});
+        }
+        private void AddHeaders()
+        {
+            summary.Add(new string[] {"Time", "Number of Products", "Cost"});
+        }
     }
 
     public class Store
     {
         private List<Product> products;
         private List<Purchase> purchases;
-
         public Store()
         {
             products = new List<Product>();
             purchases = new List<Purchase>();
         }
-
         public void AddItem(string barcode, string name, float price)
         {
             products.Add(new Product() { barcode = barcode, name = name, price = price });
         }
-
         public int ItemCount()
         {
             return products.Count();
         }
-
         public float CalculateCost(string[] barcodes)
         {
             return GetProducts(barcodes).Select(b => b.price).Sum();
         }
-
         public string PrintReceipt(string[] barcodes)
         {
             return PrintEach(barcodes) + PrintTotal(barcodes);
         }
-
         public string Purchase(string[] barcodes)
         {
-            purchases.Add(new Purchase() {
-                products     = GetProducts(barcodes),
-                purchased_at = DateTime.Now
-            }); 
+            purchases.Add(new Purchase(GetProducts(barcodes)));
             return PrintReceipt(barcodes);
         }
-
         public ArrayList PurchaseSummary()
         {
-            ArrayList summary = new ArrayList();
-            summary.Add(new string[] {"Time", "Number of Products", "Cost"});
+            var builder = new SummaryBuilder();
             foreach (Purchase p in purchases)
-            {
-                string time = String.Format("{0:MM dd YYYY}", p.purchased_at);
-                string count = "" + p.products.Count();
-                string sum = "1" + p.products.Select(b => b.price).Sum();
-                summary.Add(new string[] { time, count, sum });
-            }
-            return summary;
+                builder.Add(p.DisplayTime(), p.ProductCount(), p.Cost());
+            return builder.summary;
         }
-
         private Product[] GetProducts(string[] barcodes)
         {
             return barcodes.Select(b => GetProduct(b)).ToArray();
         }
-
         private Product GetProduct(string barcode)
         {
             return products.Find(x => x.barcode == barcode);
         }
-
         private string PrintEach(string[] barcodes)
         {
             return GetProducts(barcodes).Aggregate("", (str, p) => str + PrintItem(p));
         }
-
         private string PrintItem(Product p)
         {
             return String.Format("{0} ${1:n2}{2}", p.name, p.price, Environment.NewLine);
         }
-
         private string PrintTotal(string[] barcodes)
         {
             return String.Format("total ${0:n2}", CalculateCost(barcodes));
